68344c62831ac5b4aa5902cfda9e4a18bbbb31f1
Arafat Husayn <hello@arafat.dev>
2022-07-17 23:37:34 +0600

    update start scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-deno run --allow-env=PORT,HOSTNAME,ACCESS_TOKEN,RATE_LIMIT_WINDOW,RATE_LIMIT_MAX_REQ,TIMEOUT --allow-net mod.ts
+deno run --allow-env=PORT,HOSTNAME,ACCESS_TOKEN,RATE_LIMIT_WINDOW,RATE_LIMIT_MAX_REQ,TIMEOUT --allow-net --allow-run --allow-read=./files --allow-write=./files mod.ts
